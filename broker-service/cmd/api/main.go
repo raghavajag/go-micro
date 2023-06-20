489f6d21dfa6c4a7142b0a68fe62d6f5c513071a
raghav-crypto <raghavjgarwal@gmail.com>
2023-06-20 12:46:28 +0530

    feat: setup broker service

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8080"
+
+func main() {
+	log.Printf("Starting broker service on port %s", webPort)
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", webPort),
+	}
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
