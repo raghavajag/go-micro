ebe3b2f8aee2466ca7adada66cd531ba32ce802f
raghav-crypto <raghavjgarwal@gmail.com>
2023-06-21 16:50:14 +0530

    feat: helper function to read json from body

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 )
 
@@ -9,6 +11,26 @@ type jsonResponse struct {
 	Error   bool   `json:"error"`
 	Message string `json:"message"`
 	Data    any    `json:"data,omitempty"`
+}
+
+// readJSON tries to read the body of a request and converts it into JSON
+func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1048576 // one megabyte
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must have only a single JSON value")
+	}
+
+	return nil
 }
 
 // writeJSON takes a response status code and arbitrary data and writes a json response to the client
