f87a204c5968e4dd2ba9a42adc46ad87eca6778c
raghav-crypto <raghavjgarwal@gmail.com>
2023-07-06 23:11:50 +0530

    feat: auth-service login user

---
+++ auth-service/cmd/api/handlers.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
+	var requestPayload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		fmt.Println("error while readJSON")
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	// validate the user against the database
+	user, err := app.Models.User.GetByEmail(requestPayload.Email)
+
+	if err != nil {
+		fmt.Println("error while finding user")
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		return
+	}
+
+	valid, err := user.PasswordMatches(requestPayload.Password)
+	fmt.Println(valid)
+	fmt.Println(err)
+	if err != nil || !valid {
+		fmt.Println("error while invalidCredentials")
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusBadRequest)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: fmt.Sprintf("Logged in user %s", user.Email),
+		Data:    user,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
--- auth-service/cmd/api/routes.go
+++ auth-service/cmd/api/routes.go
@@ -20,5 +20,6 @@ func (app *Config) routes() http.Handler {
 	}))
 
 	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Post("/authenticate", app.Authenticate)
 	return mux
 }
