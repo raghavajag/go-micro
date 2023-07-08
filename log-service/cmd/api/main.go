5a80a475c993ee6e68ab94f015a0fe2a2bf1a1b4
raghav-crypto <raghavjgarwal@gmail.com>
2023-07-08 14:51:47 +0530

    log-service connecting to mongo

--- log-service/cmd/api/main.go
+++ log-service/cmd/api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
@@ -16,16 +21,56 @@ const (
 type Config struct {
 }
 
+var client *mongo.Client
+
 func main() {
 	log.Println("Starting Logger service")
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
 
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		} else {
+			log.Println("Disconnected from MongoDB")
+		}
+	}()
+	log.Println("Connected to MongoDB")
+	// err = client.Ping(ctx, nil)
+	// if err != nil {
+	// 	log.Fatal("Error pinging MongoDB:", err)
+	// }
 	app := Config{}
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic()
 	}
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+		return nil, err
+	}
+
+	return c, nil
 }
